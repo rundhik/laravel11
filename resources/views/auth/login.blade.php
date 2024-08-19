b1ffd85f9b9b5349a25b09c1a6860578458d72a3
Randy Anwar Romadhonny <rundhik@yahoo.co.id>
2024-08-19 15:09:59 +0700

    Auth Part3
    Customizing auth laravel/breeze scaffolding. :muscle: :coffee: :smoking:

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,11 +6,17 @@
             <div class="p-5">
                 <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">{{ config('app.name', 'Laravel') }} {{ __('Login') }}</h1>
+                    @if(session()->has('error'))
+                    <div class="alert alert-danger alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>{!! session('error') !!}
+                    </div>
+                    @endif
                 </div>
                 <form class="user" method="POST" action="{{ route('login') }}">
                     @csrf
                     <div class="form-group">
                         <input type="email" name="email" class="form-control form-control-user" id="email" placeholder="{{ __('Username') }}" required autofocus autocomplete="username">
+                        @error('email')<code>{{ $message }}</code>@enderror
                     </div>
                     <div class="form-group input-group mb-3">
                         <input type="password" name="password" class="form-control form-control-user" id="password" placeholder="{{ __('Password') }}">
@@ -19,6 +25,7 @@
                                 <i class="fas fa-eye-slash fa-sm" id="eye"></i>
                             </button>
                         </div>
+                        @error('password')<code>{{ $message }}</code>@enderror
                     </div>
                     <div class="form-group">
                         <div class="custom-control custom-checkbox small">
@@ -30,10 +37,12 @@
                         {{ __('Log in') }}
                     </button>
                 </form>
+                @if (Route::has('password.request'))
                 <hr>
                 <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    <a class="small" href="{{ route('password.request') }}">Forgot Password?</a>
                 </div>
+                @endif
             </div>
         </div>
     </div>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,37 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
-
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<x-auth-layout>
+    <!-- Nested Row within Card Body -->
+    <div class="row">
+        <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
+        <div class="col-lg-6">
+            <div class="p-5">
+                <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-2">{{ __('Reset Password') }}</h1>
+                    @if(session()->has('error'))
+                    <div class="alert alert-danger alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>{!! session('error') !!}
+                    </div>
+                    @endif
+                </div>
+                <form class="user" method="POST" action="{{ route('password.store') }}">
+                    @csrf
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control form-control-user" id="email" value="{{ old('email', $request->email) }}" placeholder="{{ __('Enter email address') }}" required autofocus autocomplete="username">
+                        @error('email')<code>{{ $message }}</code>@enderror
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" class="form-control form-control-user" id="password" placeholder="{{ __('Password') }}" required autocomplete="new-password">
+                        @error('password')<code>{{ $message }}</code>@enderror
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password_confirmation" class="form-control form-control-user" id="password_confirmation" placeholder="{{ __('Confirm Password') }}" required autocomplete="new-password">
+                        @error('password_confirmation')<code>{{ $message }}</code>@enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                        {{ __('Reset Password') }}
+                    </button>
+                </form>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</x-auth-layout>
